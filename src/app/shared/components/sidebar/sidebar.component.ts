ed77291e3178170e01365100a3177ede1fa0dedb
DylanVallejo <dylan1811vallejo@gmail.com>
2024-07-20 22:51:56 -0500

    adicionando dependecias para routing - creando componentes contact , y sidebar - adicionando dependencias de bootstrap - definiendo rutas para cada componente

---
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+})
+export class SideBarComponent {
+
+  public navigateHome(){
+
+  }
+
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,19 +3,32 @@ import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { AboutPageComponent } from './pages/about-page/about-page.component';
 
+import { RouterLink, RouterLinkActive, RouterModule, RouterOutlet } from '@angular/router';
+import { SideBarComponent } from './components/sidebar/sidebar.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
+
 
 
 @NgModule({
   declarations: [
     AboutPageComponent,
     HomePageComponent,
+    SideBarComponent,
+    ContactPageComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
+    // RouterLink,
+    // RouterOutlet,
+    // RouterLinkActive
   ],
   exports: [
     AboutPageComponent,
     HomePageComponent,
+    SideBarComponent,
+    ContactPageComponent
   ]
 })
 export class SharedModule { }
